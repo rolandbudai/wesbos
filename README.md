ee4b46c9a527af85474e8b8b31c3aba34d9bde31
rolandbudai <budairoland@gmail.com>
2020-03-09 22:06:39 +0100

    3
    fdh

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# wesbos  
-aysdsadasdsa
+"# wesbos" 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+import StorePicker from './StorePicker.js'
+
+
+
+render(<StorePicker />, document.getElementById('main'));
